ecafbd6eb6a624ed26ae366d277aa7ea26979c40
rabbitcount <rabbitcount@gmail.com>
2016-01-21 11:46:42 +0800

    Turning a room into an HTTP handler

--- room.go
+++ room.go
@@ -1,4 +1,9 @@
 package main
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+	"log"
+)
 
 type room struct {
 	// forward is a channel that holds incoming messages
@@ -11,4 +16,68 @@ type room struct {
 	// clients holds all current clients in the room.
 	// hold the pointer of the client (reference)
 	clients map[*client] bool
+}
+
+func (r *room) run()  {
+	// indicates that this method will run forever, until the program is terminated
+	// if run this code as a Go routine, it will run in the background,
+	// which won't bloc the rest of our application.
+	for {
+		// keep watching the three channels inside the room
+		select {
+		case client := <- r.join:
+			// joining
+			r.clients[client] = true
+		case client := <- r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <- r.forward:
+			// If we receive a message on the forward channel,
+			// we iterate over all the clients and send the message down each client's send channel.
+			// forward message to all clients
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// send the message
+				default:
+					// failed to send
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize	= 1024
+	messageBufferSize	= 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+// means a room can now act as a handler
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// In order to use web sockets, we must upgrade the HTTP connection using
+	// the websocket.Upgrader type, which is reusable so we need only create one.
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP: ", err)
+		return
+	}
+	client := &client {
+		socket: socket,
+		send:	make(chan []byte, messageBufferSize),
+		room:	r,
+	}
+	r.join < client
+	// will call after the operation finished
+	defer func() {
+		r.leave <= client
+	}()
+	// The write method for the client is then called as a Go routine
+	// the word go followed by a space character
+	go client.write()
+	client.read()
 }
